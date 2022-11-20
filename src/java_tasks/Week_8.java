15fab507f63e8955a7c7f5e5614ea18ff35f12a8
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-11-20 15:19:33 -0500

    second commit is done

---
+++ src/java_tasks/Week_8.java
@@ -0,0 +1,11 @@
+package java_tasks;
+
+public class Week_8 {
+
+    // this line is first changes
+    //second changes
+    //third changes
+    //fourth changes
+    // what will gone be hapen
+    // i am trying to learn add -p command
+}
